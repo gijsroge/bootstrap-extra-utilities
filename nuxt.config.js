d57657c078172836c93505c8ce0e92565fe05779
gijsroge <rogegijs@gmail.com>
2018-10-25 00:18:44 +0200

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,65 @@
+const pkg = require('./package')
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      //{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Ubuntu:400,700' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    "@/assets/scss/main.scss",
+    "@/assets/scss/bootstrap.scss",
+    "@/assets/scss/bootstrap-utilities.scss"
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    [
+      'nuxt-sass-resources-loader',
+      [
+        '@/assets/scss/_customize-bootstrap.scss',
+      ]
+    ]
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="container">
+    <h1 class="z-index-lg-10 overflow-auto">Bootstrap Utilities</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  components: {}
+};
+</script>
+
+<style>
+</style>
